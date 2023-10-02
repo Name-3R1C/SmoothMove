0558d20021e0272e269761a6d4d0cc7d56b041aa
PlumScum <jelanisands@gmail.com>
2023-10-01 23:14:11 -0400

    Correct spacing, import scss

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,53 +1,55 @@
 import React from 'react';
+import '.Footer.scss';
 
 function Footer() {
-  return (
-    <footer>
+return (
+    <footer className="footer">
       {/* First Section: Logo */}
-      <div className="logo">
-        <img src="your-logo.png" alt="SmoothMove.INC Logo" />
-      </div>
+        <div className="logo">
+        <span className="smooth">Smooth</span>
+        <span className="move-inc">Move.INC</span>
+        </div>
 
       {/* Second Section: Categories */}
-      <div className="categories">
+        <div className="categories">
         <div className="category">
-          <h4>Crypto</h4>
-          <ul>
+            <h4>Crypto</h4>
+        <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Buy NFTs</a></li>
             <li><a href="#">Sell NFTs</a></li>
-          </ul>
+        </ul>
         </div>
         <div className="category">
-          <h4>Market</h4>
-          <ul>
+            <h4>Market</h4>
+            <ul>
             <li><a href="#">Browse NFTs</a></li>
             <li><a href="#">Buy NFTs</a></li>
             <li><a href="#">Sell NFTs</a></li>
-          </ul>
+            </ul>
         </div>
         <div className="category">
-          <h4>Contact</h4>
-          <ul>
+            <h4>Contact</h4>
+        <ul>
             <li><a href="#">Email</a></li>
             <li><a href="#">LinkedIn</a></li>
             <li><a href="#">Instagram</a></li>
             <li><a href="#">Twitter</a></li>
-          </ul>
+        </ul>
         </div>
-      </div>
+    </div>
 
       {/* Third Section: Newsletter */}
-      <div className="newsletter">
+    <div className="newsletter">
         <h4>Join Our Newsletter</h4>
         <div className="email-container">
-          <input type="email" placeholder="Enter your email" />
-          <button type="submit">Subscribe</button>
+        <input type="email" placeholder="Enter your email" />
+        <button type="submit">Subscribe</button>
         </div>
-      </div>
+    </div>
     </footer>
-  );
+);
 }
 
 export default Footer;
