a4d1baac79f5a65701ae96bb65e0562a8e7faf6e
shovon231 <shovon231@gmail.com>
2023-10-07 14:04:06 -0400

    adding hamburger menu to the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,70 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar({ setPage, userName, logout }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  const buttonStyles = {
+    backgroundColor: "transparent", // Set the background color to transparent
+    border: "none", // Remove border if needed
+    padding: 0, // Remove padding
+    width: "auto", // Allow button to adjust to content
+  };
   return (
-    <nav className="navbar navbar-expand navbar-light bg-light">
-      <a className="navbar-brand" onClick={() => setPage("PropertyList")}>
+    <nav className="navbar">
+      <div className="navbar-brand" onClick={() => setPage("PropertyList")}>
         <span className="name_red">
           <FontAwesomeIcon icon={faHouse} />
           Smooth
         </span>
-        MOVE
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <a
-            className="nav-item nav-link active"
-            onClick={() => setPage("PropertyList")}
-          >
-            Home
-          </a>
-          <a
-            className="nav-item nav-link"
-            onClick={() => {
-              if (userName) {
-                setPage("AddProperty");
-              } else {
-                window.alert("Please log in to add a property.");
-                setPage("LoginSignup");
-              }
-            }}
-          >
-            Add Property
-          </a>
+        MOVE.INC
+      </div>
 
-          {userName ? (
-            <div className="user_status">
-              <a className="nav-item nav-link user_name">
-                Welcome: {userName.firstName}
-              </a>
-              <a className="nav-item nav-link logout" onClick={logout}>
-                Logout
-              </a>
-            </div>
-          ) : (
-            <a
-              className="nav-item nav-link"
-              onClick={() => setPage("LoginSignup")}
-            >
-              Login/Signup
+      <div class="dropdown">
+        <button
+          // class="btn btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+          style={buttonStyles}
+        >
+          <div
+            className={`menu-icon ${menuOpen ? "open" : ""}`}
+            onClick={toggleMenu}
+          >
+            {menuOpen ? (
+              <FontAwesomeIcon icon={faTimes} size="lg" />
+            ) : (
+              <FontAwesomeIcon icon={faBars} size="lg" />
+            )}
+          </div>
+        </button>
+        <ul class="dropdown-menu dropdown-menu-end">
+          <li>
+            <a class="dropdown-item" href="#">
+              Action
             </a>
-          )}
-        </div>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Another action
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Something else here
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
