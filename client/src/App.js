998b60149c3a87cb693c158e61ef82e051c3a758
PlumScum <jelanisands@gmail.com>
2023-10-07 22:50:10 -0400

    Add import, container for Header/footer

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import Navbar from "./components/Navbar";
 import LoginSignup from "./components/LoginSignup";
 import PropertySearch from "./components/PropertySearch";
 
+// // Importing finished components here!
+// import Header from "./components/Header"; // Adjust the import path as needed
+// import Footer from "./components/Footer"; // Adjust the import path as needed
+
 const pages = {
   PropertyList: "PropertyList",
   PropertyDetail: "PropertyDetail",
@@ -26,12 +30,16 @@ function App() {
   };
 
   return (
-    <main className="layout">
-      <Navbar setPage={setPage} userName={cookies.user} logout={handleLogout} />
-      {page === pages.AddProperty && <AddProperty />}
-      {page === pages.LoginSignup && <LoginSignup onLogin={handleLogin} />}
-      {page === pages.PropertyList && <PropertySearch />}
-    </main>
+    <div className="app">
+      <Header /> {/* JayS is currently working on the header component, to be stored here */}
+      <main className="layout">
+        <Navbar setPage={setPage} userName={cookies.user} logout={handleLogout} />
+        {page === pages.AddProperty && <AddProperty />}
+        {page === pages.LoginSignup && <LoginSignup onLogin={handleLogin} />}
+        {page === pages.PropertyList && <PropertySearch />}
+      </main>
+      <Footer /> {/* JayS is currently working on the footer component, to be stored here */}
+    </div>
   );
 }
 
