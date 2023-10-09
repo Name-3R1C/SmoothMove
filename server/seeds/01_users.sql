f654a42fddc5367725c078cd22b0ee9f9f25c6cb
Name-3R1C <ericfeng94@hotmail.com>
2023-10-09 02:48:41 -0700

    add more property, chagne all users pwd to 123

--- server/seeds/01_users.sql
+++ server/seeds/01_users.sql
@@ -1,4 +1,7 @@
+-- password 123
 INSERT INTO users (first_name, last_name, email, password)
-VALUES ('Eva', 'Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), 
-('Louisa', ' Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), 
-('Dominic', ' Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+VALUES 
+('Admin', 'User', 'admin@mail.com', '$2a$10$AAU4DMvMC5OMSXPEWxC69OXTqgU7FYUpT9XBTpXKAcV8JG1L7kIl.'), 
+('Eva', 'Stanley', 'sebastianguerra@ymail.com', '$2a$10$AAU4DMvMC5OMSXPEWxC69OXTqgU7FYUpT9XBTpXKAcV8JG1L7kIl.'), 
+('Louisa', ' Meyer', 'jacksonrose@hotmail.com', '$2a$10$AAU4DMvMC5OMSXPEWxC69OXTqgU7FYUpT9XBTpXKAcV8JG1L7kIl.'), 
+('Dominic', ' Parks', 'victoriablackwell@outlook.com', '$2a$10$AAU4DMvMC5OMSXPEWxC69OXTqgU7FYUpT9XBTpXKAcV8JG1L7kIl.');
