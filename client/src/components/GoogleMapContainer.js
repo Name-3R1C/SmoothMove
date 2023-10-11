e0ae98c581ba4549b27e67d8f1df20c4f9d19de4
PlumScum <jelanisands@gmail.com>
2023-10-10 22:57:15 -0400

    Initial commit

---
+++ client/src/components/GoogleMapContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GoogleMap from "./GoogleMap";
+
+function GoogleMapContainer() {
+return (
+    <div className="google-map-container">
+    <GoogleMap />
+    </div>
+);
+}
+
+export default GoogleMapContainer;
